d66ea1cb4a665b7271fe8c084e829a60acc61c23
daudwee <daudwee@gmail.com>
2011-02-26 18:19:41 +1100

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daudwee::Application.config.secret_token = '1f38182d0dc8e77262cf7a730789bfedf16ee5c0421e683e051328208599d12d8aa480e44ccb480430034025e336c31d3e267a3b64e530ca381239419c15fdf4'
